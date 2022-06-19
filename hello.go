5b58b5262e5c4d74079e0a470c65baaf48e51044
Micah Kimel <micahkimel@Micahs-MacBook-Pro.local>
2022-06-19 15:41:43 -0400

    test out docker instance of mysql

--- hello.go
+++ hello.go
@@ -1,44 +1,68 @@
 package main
 import (
-    "fmt"
-    "context"
-	"net/http"
-	"io/ioutil"
-    "github.com/go-redis/redis/v8"
+	//"fmt"
+    "database/sql"
+
+    _ "github.com/go-sql-driver/mysql"
+    // "fmt"
+    // "context"
+	// "net/http"
+	// "io/ioutil"
+    // "github.com/go-redis/redis/v8"
 )
 
-var ctx = context.Background()
+//var ctx = context.Background()
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request){
-		d, _ := ioutil.ReadAll(r.Body)
-		rdb := redis.NewClient(&redis.Options{
-			Addr:     "localhost:6379",
-			Password: "", // no password set
-			DB:       0,  // use default DB
-		})
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/db")
 
-		err := rdb.Set(ctx, "key", d, 0).Err()
-		if err != nil {
-			panic(err)
-		}
+	// if there is an error opening the connection, handle it
+    if err != nil {
+        panic(err.Error())
+    }
 
-		val, err := rdb.Get(ctx, "key").Result()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("key", val)
-		fmt.Fprintf(rw, "Hello %s\n", d)
+    // defer the close till after the main function has finished
+    // executing
+    defer db.Close()
 
-		val2, err := rdb.Get(ctx, "key2").Result()
-		if err == redis.Nil {
-			fmt.Println("key2 does not exist")
-		} else if err != nil {
-			panic(err)
-		} else {
-			fmt.Println("key2", val2)
-    }})
-	http.ListenAndServe(":9090",nil)
+	insert, err := db.Query("call db.create_user('Test', '123', 'TEST', 'TETS' )")
+
+	if err != nil {
+        panic(err.Error())
+    }
+    // be careful deferring Queries if you are using transactions
+    defer insert.Close()
+
+
+	// http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request){
+	// 	d, _ := ioutil.ReadAll(r.Body)
+	// 	rdb := redis.NewClient(&redis.Options{
+	// 		Addr:     "localhost:6379",
+	// 		Password: "", // no password set
+	// 		DB:       0,  // use default DB
+	// 	})
+
+	// 	err := rdb.Set(ctx, "key", d, 0).Err()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	val, err := rdb.Get(ctx, "key").Result()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println("key", val)
+	// 	fmt.Fprintf(rw, "Hello %s\n", d)
+
+	// 	val2, err := rdb.Get(ctx, "key2").Result()
+	// 	if err == redis.Nil {
+	// 		fmt.Println("key2 does not exist")
+	// 	} else if err != nil {
+	// 		panic(err)
+	// 	} else {
+	// 		fmt.Println("key2", val2)
+    // }})
+	// http.ListenAndServe(":9090",nil)
 }
 
 // import (
